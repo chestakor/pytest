9d8cbb5714a23bf12607f2e2712f33bb2b038fbb
chestakor <139700193+chestakor@users.noreply.github.com>
2024-07-01 01:52:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import str1  # Example for /chk command
 import seedr  # Example for /seedr command
 import nonsk1  # Example for /nonsk1 command
 import hoi  # Example for /hoi command
+import crunchy
 
 bot = telebot.TeleBot('7237381740:AAGoGZZKQjYUkHBJWd56Xb0fAxJExylP5f0')
 
@@ -31,6 +32,11 @@ def check_nonsk1_command(message):
 def check_hoi_command(message):
     chat_id = message.chat.id
     hoi.process_hoi_command(bot, message)
+
+@bot.message_handler(commands=['crunchy'])
+def check_crunchy_command(message):
+    chat_id = message.chat.id
+    crunchy.process_crunchy_command(bot, message)
 
 if __name__ == "__main__":
     keep_alive()
