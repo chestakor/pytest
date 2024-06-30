d19741bd2b963f684816aa51c523fa1c9c631138
chestakor <139700193+chestakor@users.noreply.github.com>
2024-06-30 21:13:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import telebot
 from telebot import types
 from keep_alive import keep_alive
-import str1  # Import the new str1.py
+import str1  # Import str1.py for the /chk command
+import seedr  # Import seedr.py for the /seedr command
 
 bot = telebot.TeleBot('7237381740:AAGoGZZKQjYUkHBJWd56Xb0fAxJExylP5f0')
 
@@ -14,6 +15,11 @@ def send_welcome(message):
 def check_card_command(message):
     chat_id = message.chat.id
     str1.process_chk_command(bot, message)
+
+@bot.message_handler(commands=['seedr'])
+def check_seedr_command(message):
+    chat_id = message.chat.id
+    seedr.process_seedr_command(bot, message)
 
 if __name__ == "__main__":
     keep_alive()
