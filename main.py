a1534255a1807714446caa2ab405c69231911ab0
chestakor <139700193+chestakor@users.noreply.github.com>
2024-07-01 18:35:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ bot = telebot.TeleBot('7237381740:AAGoGZZKQjYUkHBJWd56Xb0fAxJExylP5f0')
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     chat_id = message.chat.id
+    gif_url = 'https://media.gifdb.com/iron-man-welcome-zbqyitqbuflw2oq6.gif'
+    bot.send_animation(chat_id, gif_url)
     bot.send_message(chat_id, "Hello, sir!\nUse /help to know all command\nOwner: Aftab👑")
 
 @bot.message_handler(commands=['chk'])
