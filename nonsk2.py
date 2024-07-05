201dc9db74949121481aac72e5dd1c8f4b9ac872
chestakor <139700193+chestakor@users.noreply.github.com>
2024-07-06 01:42:13 +0600

    Update nonsk2.py

--- nonsk2.py
+++ nonsk2.py
@@ -57,6 +57,7 @@ def check_nonsk2_card(cc):
         "card[number]": cc_number,
         "card[exp_month]": exp_month,
         "card[exp_year]": exp_year,
+        "card[cvc]": cvc,
         "guid": generate_guid(),
         "muid": generate_guid(),
         "sid": generate_guid(),
