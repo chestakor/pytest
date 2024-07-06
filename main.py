f97907c25ebf368734d68aa3c7a90874c4beb766
chestakor <139700193+chestakor@users.noreply.github.com>
2024-07-06 18:19:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import grab
 import nonsk2
 import help
 import juicy
-import crunchytxt
 import nonsk3
 import nonsk4
 from telebot import types
@@ -20,7 +19,8 @@ import address
 import gen
 import bin
 import sk
-import hoitxt
+from hoitxt import process_hoitxt_command, handle_hoitxt_docs, handle_hoitxt_callback_query
+from crunchytxt import process_crunchytxt_command, handle_crunchytxt_docs, handle_crunchytxt_callback_query
 import info
 import nord
 import panda
@@ -143,33 +143,43 @@ def nubile_command(message):
 def handle_juicy_command(message):
     juicy.process_juicy_command(bot, message)
 
+current_command = None
+
+# Handler for /hoitxt command
 @bot.message_handler(commands=['hoitxt'])
 def handle_hoitxt_command(message):
-    hoitxt.process_hoitxt_command(bot, message)
+    global current_command
+    current_command = 'hoitxt'
+    process_hoitxt_command(bot, message)
+
+# Handler for /crunchytxt command
+@bot.message_handler(commands=['crunchytxt'])
+def handle_crunchytxt_command(message):
+    global current_command
+    current_command = 'crunchytxt'
+    process_crunchytxt_command(bot, message)
 
 # Handler for document upload
 @bot.message_handler(content_types=['document'])
 def handle_docs(message):
-    hoitxt.handle_docs(bot, message)
+    global current_command
+    if current_command == 'hoitxt':
+        handle_hoitxt_docs(bot, message)
+    elif current_command == 'crunchytxt':
+        handle_crunchytxt_docs(bot, message)
+    else:
+        bot.send_message(message.chat.id, "Please specify the command (/hoitxt or /crunchytxt) first.")
 
-# Handler for callback queries
+# Handler for callback queries for both commands
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
-    hoitxt.handle_callback_query(call, bot)
-
-@bot.message_handler(commands=['crunchytxt'])
-def handle_crunchytxt_command(message):
-    crunchytxt.process_crunchytxt_command(bot, message)
-
-# Handler for document upload specific to crunchytxt
-@bot.message_handler(content_types=['document'])
-def handle_docs(message):
-    crunchytxt.handle_docs(bot, message)
-
-# Handler for callback queries specific to crunchytxt
-@bot.callback_query_handler(func=lambda call: True)
-def handle_callback_query(call):
-    crunchytxt.handle_callback_query(call, bot)
+    global current_command
+    if current_command == 'hoitxt':
+        handle_hoitxt_callback_query(call, bot)
+    elif current_command == 'crunchytxt':
+        handle_crunchytxt_callback_query(call, bot)
+    else:
+        bot.send_message(call.message.chat.id, "Unrecognized callback query.")
 
 @bot.message_handler(commands=['nagad'])
 def nagad_command(message):
